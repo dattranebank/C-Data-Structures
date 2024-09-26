8d3e8d3b59e03a552f3d2e1eacab27623e45b96b
Dat Ebank <128462139+dattranebank@users.noreply.github.com>
2024-09-26 12:12:48 +0700

    Nhập xuất Vector cơ bản (Cách 1)

---
+++ 2. Vector/1. Vector_Cach1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector> // Khai báo thư viện vector
+using namespace std;
+
+int main()
+{
+    int n;
+    cout <<"Nhap so phan tu:"; cin >>n;
+    vector <int> vec; // Tạo vector vec
+
+    // Nhập vector
+    int tmp; // Tạo biến tạm để nhập vector
+    for (int i=0;i<n;i++)
+    {
+        cout <<"vec["<<i<<"]:"; cin >>tmp;
+        vec.push_back(tmp);
+    }
+
+    // Xuất vector
+    cout <<"Output: ";
+    for (int i=0;i<n;i++)
+    {
+        cout <<vec[i]<<" ";
+    }
+}
